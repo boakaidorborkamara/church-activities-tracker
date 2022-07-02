f2f104a4b0dfbd6cb9b46815b85319575c1b4c58
Boakai <boakaidkamara@gmail.com>
2022-07-02 01:13:35 +0000

    linked controller and router files for user model

--- server.js
+++ server.js
@@ -18,15 +18,17 @@ const attendance_table = require('./model/attendance-table');
 
 //ROUTES
 const index_router = require('./routers/index_routers');
-const church_routers = require('./routers/church_routers');
-const event_routers = require("./routers/event_router");
+const church_router = require('./routers/church_routers');
+const event_router = require("./routers/event_router");
 const role_router = require('./routers/role_router');
+const user_router = require('./routers/user_router')
 
 
 app.use(index_router);
-app.use(church_routers);
-app.use(event_routers);
+app.use(church_router);
+app.use(event_router);
 app.use(role_router);
+app.use(user_router);
 
 
 
