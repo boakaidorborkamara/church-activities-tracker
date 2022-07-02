b4657440591fb41a343692bcce54eb394a496ca2
Boakai <boakaidkamara@gmail.com>
2022-07-02 00:24:21 +0000

    linked church controllers with routes

--- routers/church_routers.js
+++ routers/church_routers.js
@@ -9,5 +9,13 @@ const router = express.Router();
 //get all churches
 router.get('/churches', churchController.church_list);
 
+//create a new book POST
+router.post('/church/create', churchController.church_create_post);
+
+//display details for a specific route
+router.get('/church/:id', churchController.church_detail)
+
+
+
 
 module.exports = router;
